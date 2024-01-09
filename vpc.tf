a6fb2e7c70fa192a534c4e218ce909d3888bb145
mcchandu <43166150+mcchandu@users.noreply.github.com>
2024-01-09 16:06:04 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = "myVPC"
   cidr = "10.0.0.0/16"
 
-  azs             = ["ap-south-1b"]
+  azs             = ["us-east-1a"]
   private_subnets = ["10.0.1.0/24"]
   public_subnets  = ["10.0.101.0/24"]
 }
